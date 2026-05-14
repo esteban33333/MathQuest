f5b5770da66dd39ef2ce46b26d61e9fc94241c6d
Estebantrabajofinal <esteban.aguilar@tecsup.edu.pe>
2026-05-14 16:10:59 -0500

    Initial Commit

---
+++ MathQuest/MathQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathQuestApp.swift
+//  MathQuest
+//
+//  Created by Esteban Aguilar  on 14/05/26.
+//
+
+import SwiftUI
+
+@main
+struct MathQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
